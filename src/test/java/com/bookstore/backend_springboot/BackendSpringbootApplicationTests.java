f18b4ff28de0e8733f554a364c88786e29307d27
Dang The Hao <dangthehao28@gmail.com>
2025-03-25 10:25:59 +0700

    first commit

---
+++ src/test/java/com/bookstore/backend_springboot/BackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.backend_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
