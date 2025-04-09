a580124f83a43f6be9dfcebe45b03e44c62b9dd2
Dang The Hao <dangthehao28@gmail.com>
2025-04-09 22:15:36 +0700

    feat: config Spring Security with Basic Auth

---
+++ src/main/java/com/bookstore/backend_springboot/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.bookstore.backend_springboot.repository;
+
+import com.bookstore.backend_springboot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
